05f208b778298173a8ca1b7fbb8b6ec9bbac4d75
Aman Kumar Gupta <akgbytes@gmail.com>
2025-08-28 22:37:12 +0530

    feat: setup frontend with react-router, better-auth and shadcn

---
+++ frontend/src/components/login-form.tsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { toast } from "sonner";
+
+import { FaGoogle } from "react-icons/fa";
+import { authClient } from "@/lib/auth-client";
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const onSocial = (provider: "google") => {
+    setLoading(true);
+    authClient.signIn.social(
+      {
+        provider,
+        callbackURL: "/",
+      },
+      {
+        onSuccess: () => {
+          setLoading(false);
+          navigate("/");
+          toast.success("Logged in successfully");
+        },
+        onError: () => {
+          setLoading(false);
+          toast.error("Failed to login");
+        },
+      }
+    );
+  };
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader className="text-center">
+          <CardTitle className="text-xl">Welcome back</CardTitle>
+          <CardDescription>Login with your Google account</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid gap-6">
+              <div className="flex flex-col gap-4">
+                <Button
+                  variant="secondary"
+                  className="w-full cursor-pointer"
+                  onClick={() => {
+                    onSocial("google");
+                  }}
+                  disabled={loading}
+                >
+                  <FaGoogle className="size-4" />
+                  Login with Google
+                </Button>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
